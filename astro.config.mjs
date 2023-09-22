a93dadc45581bea4c0782b05a532437787243413
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2023-09-22 13:56:12 +0300

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import react from "@astrojs/react";
+import remarkMermaid from "remark-mermaidjs";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://interledger.tech",
+  markdown: {
+    remarkPlugins: [remarkMermaid],
+  },
+  integrations: [
+    starlight({
+      title: "Docs style guide",
+      customCss: [
+        "./node_modules/@interledger/docs-design-system/src/styles/green-theme.css",
+        "./node_modules/@interledger/docs-design-system/src/styles/ilf-docs.css",
+      ],
+      social: {
+        github: "https://github.com/interledger",
+      },
+      sidebar: [
+        {
+          label: "Content",
+          autogenerate: {
+            directory: "content",
+          },
+        },
+        {
+          label: "Components",
+          autogenerate: {
+            directory: "components",
+          },
+        },
+      ],
+    }),
+    react(),
+  ],
+  server: {
+    port: 1105,
+  },
+});
