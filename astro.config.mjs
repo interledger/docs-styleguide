cf43dc5edef8566f279daa7e9525ae702a79e081
Melissa Henderson <57110301+melissahenderson@users.noreply.github.com>
2025-03-12 08:55:26 -0400

    docs: vale vocab file and ext links page

--- astro.config.mjs
+++ astro.config.mjs
@@ -44,7 +44,7 @@ export default defineConfig({
           },
         },
         {
-          label: "Classes, styles, HTML",
+          label: "Classes, styles, HTML, etc",
           autogenerate: {
             directory: "classes",
           },
