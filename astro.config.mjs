17d56d6c60df846e0879e8a1adda78c8b6b5e3cb
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2025-05-14 19:24:13 +0800

    feat: add collapsible sidebars component (#77)
    * feat: add collapsible sidebars component
    
    * feat: modify implementation based on feedback
    
    * feat: revert back to 2 button implementation
    
    * trigger deploy preview
    
    * install and implement starlight fullview mode plugin
    
    * update to dds 0.7.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from "@astrojs/starlight";
 import remarkMath from "remark-math";
 import rehypeMathjax from "rehype-mathjax";
 import starlightLinksValidator from "starlight-links-validator";
+import starlightFullViewMode from "starlight-fullview-mode";
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,7 +20,7 @@ export default defineConfig({
         "./node_modules/@interledger/docs-design-system/src/styles/teal-theme.css",
         "./node_modules/@interledger/docs-design-system/src/styles/ilf-docs.css",
       ],
-      plugins: [starlightLinksValidator()],
+      plugins: [starlightLinksValidator(), starlightFullViewMode()],
       expressiveCode: {
         styleOverrides: {
           borderColor: "transparent",
